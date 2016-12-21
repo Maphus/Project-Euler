e34f48e483737607f2df682a7f9f6a753a25c786
Maphus <pgrant1225@yahoo.com>
2016-12-20 23:49:43 -0500

    started 5

--- p_5.py
+++ p_5.py
@@ -5,9 +5,10 @@ vrai = 0
 while vrai == 0:
     j = 1
     while j < 21:
-        vrai = 1
         if i % j != 0:
             vrai = 0
+            break
+        vrai = 1
         j+=1
     i += 1
 
